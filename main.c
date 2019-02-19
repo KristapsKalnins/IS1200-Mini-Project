07e64472e0e9a6286a15cde658228cd34d1e0d73
Kristaps <kristapskalnin@gmail.com>
2019-02-19 13:04:00 +0100

    Riņķa kustība

--- main.c
+++ main.c
@@ -4,24 +4,28 @@
 #include "assets.h"
 
 #define PADDLE_COLOR YELLOW
-#define BG_COLOR WHITE
+#define BG_COLOR BLACK
 int main(void){
     display_init();
     rotate(1);
     fillSceen(BG_COLOR);
     while(1){
-        drawCircle(100, 100, 10, BLUE);
         int i;
-        for(i = 0; i < 190; i++){
-            fillRect(i+50, 250, 1, 10, PADDLE_COLOR);
-            delay_ms(20);
-            fillRect(i, 250, 1, 10, BG_COLOR);
+        for(i = 0; i < 240; i++){
+        drawCircle(i, i, 10, WHITE);
+        delay_ms(50);
+        drawCircle(i-1, i-1, 10, BG_COLOR);
         }
-        for(i = 190; i > 0; i--){
-            fillRect(i, 250, 1, 10, PADDLE_COLOR);
-            delay_ms(20);
-            fillRect(i + 50, 250, 1, 10, BG_COLOR);
-        }
+        // for(i = 0; i < 190; i++){
+        //     fillRect(i+50, 250, 1, 10, PADDLE_COLOR);
+        //     delay_ms(20);
+        //     fillRect(i, 250, 1, 10, BG_COLOR);
+        // }
+        // for(i = 190; i > 0; i--){
+        //     fillRect(i, 250, 1, 10, PADDLE_COLOR);
+        //     delay_ms(20);
+        //     fillRect(i + 50, 250, 1, 10, BG_COLOR);
+        // }
         //drawBitmap(icon);
     }
     return 0;
